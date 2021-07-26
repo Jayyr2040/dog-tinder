5735a5f30842671850cbc060a09492b2ce40afb7
Sylvester <sylvesterhoe@gmail.com>
2021-07-27 00:00:46 +0800

    feat: 🎸 Added image upload to cloudinary for user avatars
    Able to perform image uploads using the same logic as dogs, added some
    design to the forms

--- client/src/components/register/AddDog.js
+++ client/src/components/register/AddDog.js
@@ -4,19 +4,27 @@ import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Grid } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(1),
+    padding: 40,
+  },
   form: {
     marginTop: 20,
   },
   field: {
     marginTop: 10,
   },
-});
+}));
 
 export default function AddDog(props) {
   const classes = useStyles();
-  const [uploadedImage, setUploadedImage] = useState("https://image.flaticon.com/icons/png/512/1581/1581594.png");
+  const [uploadedImage, setUploadedImage] = useState(
+    "https://image.flaticon.com/icons/png/512/1581/1581594.png"
+  );
   const [dogData, setDogData] = useState({ owner: props.userId });
   const [buttonState, setButtonState] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -90,7 +98,7 @@ export default function AddDog(props) {
       const files = e.target.files;
       const data = new FormData();
       data.append("file", files[0]);
-      data.append("upload_preset", "dogtinder");
+      data.append("upload_preset", "dog_tinder_dogs");
       setLoading(true);
 
       const res = await fetch(
@@ -113,87 +121,103 @@ export default function AddDog(props) {
 
   return (
     <Container>
-      <h2>Add your dog</h2>
-      <form className={classes.form} onSubmit={handleSubmit}>
-        <input
-          type="file"
-          name="file"
-          placeholder="Upload an image"
-          onChange={uploadImage}
-        />
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          <img
-            src={uploadedImage}
-            style={{ height: "280px", width: "280px" }}
-            alt=""
-          />
-        )}
-        <TextField
-          onChange={(e) => setDogData({ ...dogData, name: e.target.value })}
-          label="Name of Doggo"
-          variant="outlined"
-          error={dogNameError}
-          fullWidth
-          className={classes.field}
-        />
-        {/* <TextField
-          onChange={(e) => setDogData({ ...dogData, image: e.target.value })}
-          label="Image"
-          className={classes.field}
-          variant="outlined"
-          error={dogImageError}
-          fullWidth
-        /> */}
-        <TextField
-          onChange={(e) => setDogData({ ...dogData, breed: e.target.value })}
-          label="Breed"
-          variant="outlined"
-          error={breedError}
-          className={classes.field}
-          fullWidth
-        />
-        <TextField
-          onChange={(e) => setDogData({ ...dogData, sex: e.target.value })}
-          label="Sex"
-          variant="outlined"
-          error={sexError}
-          className={classes.field}
-          fullWidth
-        />
-        <TextField
-          onChange={(e) => setDogData({ ...dogData, yob: e.target.value })}
-          label="Year of Birth"
-          variant="outlined"
-          error={yobError}
-          className={classes.field}
-          type="number"
-          fullWidth
-        />
-        <TextField
-          onChange={(e) =>
-            setDogData({ ...dogData, description: e.target.value })
-          }
-          label="Description"
-          variant="outlined"
-          fullWidth
-          multiline
-          rows={3}
-          className={classes.field}
-          error={descriptionError}
-        />
-        <Button
-          type="submit"
-          color="secondary"
-          variant="contained"
-          size="large"
-          className={classes.field}
-          disabled={buttonState}
-        >
-          Add dog
-        </Button>
-      </form>
+      <Grid
+        container
+        spacing={4}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={4} lg={4}>
+          <Paper elevation={5} className={classes.paper}>
+            <h2>Add your dog</h2>
+            <form className={classes.form} onSubmit={handleSubmit}>
+              {loading ? (
+                <CircularProgress />
+              ) : (
+                <img
+                  src={uploadedImage}
+                  style={{ height: "280px", width: "280px" }}
+                  alt=""
+                />
+              )}
+              <Button variant="contained" component="label">
+                Upload File
+                <input
+                  type="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  onChange={uploadImage}
+                  hidden
+                />
+              </Button>
+              <TextField
+                onChange={(e) =>
+                  setDogData({ ...dogData, name: e.target.value })
+                }
+                label="Name of Doggo"
+                variant="outlined"
+                error={dogNameError}
+                fullWidth
+                className={classes.field}
+              />
+              <TextField
+                onChange={(e) =>
+                  setDogData({ ...dogData, breed: e.target.value })
+                }
+                label="Breed"
+                variant="outlined"
+                error={breedError}
+                className={classes.field}
+                fullWidth
+              />
+              <TextField
+                onChange={(e) =>
+                  setDogData({ ...dogData, sex: e.target.value })
+                }
+                label="Sex"
+                variant="outlined"
+                error={sexError}
+                className={classes.field}
+                fullWidth
+              />
+              <TextField
+                onChange={(e) =>
+                  setDogData({ ...dogData, yob: e.target.value })
+                }
+                label="Year of Birth"
+                variant="outlined"
+                error={yobError}
+                className={classes.field}
+                type="number"
+                fullWidth
+              />
+              <TextField
+                onChange={(e) =>
+                  setDogData({ ...dogData, description: e.target.value })
+                }
+                label="Description"
+                variant="outlined"
+                fullWidth
+                multiline
+                rows={3}
+                className={classes.field}
+                error={descriptionError}
+              />
+              <Button
+                type="submit"
+                color="secondary"
+                variant="contained"
+                size="large"
+                className={classes.field}
+                disabled={buttonState}
+              >
+                Add dog
+              </Button>
+            </form>
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
