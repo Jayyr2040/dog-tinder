de36a9766abbea9d313cd142918b2cf27856236a
jaytai2040 <jayyr2040@gmail.com>
2021-07-26 17:13:00 +0800

    feat: 🎸 Testing log in and find dogs from owners id

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import { Route, Link, Redirect, Switch } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <h1>Dog Tinder</h1>
       <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
       <Switch>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
       </Switch>
     </div>
---
+++ client/src/pages/Login.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import NewSessions from "../components/NewSessions";
+import "../App.css";
+
+function Login() {
+
+
+  return (
+    <div className="App">
+        <NewSessions/>
+    </div>
+  );
+}
+
+export default Login;
