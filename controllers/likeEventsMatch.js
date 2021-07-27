15b85bf0760502e93374d6b1ecdbf9ef860ede2d
edangx100 <edangx20@outlook.com>
2021-07-28 01:43:13 +0800

    complete remaining likeEvent match route

--- controllers/likeEventsMatch.js
+++ controllers/likeEventsMatch.js
@@ -17,8 +17,7 @@ router.post("/", (req, res) => {
     // console.log( req.body );
 
     let matchArray = [];
-    let OWNER_DOG_ID;
-    let dogsThatUserDogLikes;
+    let OWNER_DOGS_IDS;
 
     // let OWNER_ID = req.session.currentUser._id;          // Matching likeEvent (not tested yet with sessions)
     let OWNER_ID = "60fe823434ff591296b78d16";              // hardcoded
@@ -31,26 +30,43 @@ router.post("/", (req, res) => {
 
     // get id of owner's dogs 
     const results2A = (ownerDogs) => {
-        let dogsIdArray = [];
-        ownerDogs.forEach(element => dogsIdArray.push( element._id ));
-        return dogsIdArray;
+        let ownerDogsIds = [];
+        ownerDogs.forEach(element => ownerDogsIds.push( element._id ));
+        OWNER_DOGS_IDS = ownerDogsIds;
+        return ownerDogsIds;
     }
 
     // find likeEvent where other owners like owner's dogs
-    const results2B = (dogsIdArray) =>
-        LikeEvent.find({likee: {$in: dogsIdArray}}, (error, dogMatches)=> {    
+    const results2B = (ownerDogsIds) =>
+        LikeEvent.find({likee: {$in: ownerDogsIds}}, (error, otherOwnerDogs)=> {    
+            return otherOwnerDogs;
+        })
+
+    // get id of other owners's dog
+    const results3A = (otherOwnerDogs) => {
+        let otherOwnerDogsIds = [];
+        otherOwnerDogs.forEach(element => otherOwnerDogsIds.push( element.liker ));
+        return otherOwnerDogsIds;
+    }
+
+    // likeEvent matches = owner's dog also like these set of other owners's dog
+    const results3B = (otherOwnerDogsIds) => 
+        LikeEvent.find({ $and: [ { liker: {$in: OWNER_DOGS_IDS} }, {likee: {$in: otherOwnerDogsIds}} ]  }, (error, dogMatches)=> {    
             return dogMatches;
         })
 
     results1()
         .then(results2A )
             .then(results2B )
-                .then(  (matchArray) =>{
-                    console.log(matchArray);
-                    res.send(matchArray);
-                })
-
+                .then(results3A )
+                    .then(results3B )
+                        .then(  (matchArray) =>{
+                            console.log(matchArray);
+                            res.send(matchArray);
+                        })
+    
 });
 
-
 module.exports = router;
+
+
