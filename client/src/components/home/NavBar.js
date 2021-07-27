16b999d98f8f59a3835995211323305b090e1fcf
Wee Jerrick <weejerrick@gmail.com>
2021-07-28 00:28:45 +0800

    feat: 🎸 Show user image on avatar on nav bar

--- client/src/components/home/NavBar.js
+++ client/src/components/home/NavBar.js
@@ -6,9 +6,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { IconButton } from "@material-ui/core";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import { Menu } from "@material-ui/core";
 import { MenuItem } from "@material-ui/core";
+import { Avatar } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -42,7 +43,6 @@ export default function NavBar(props) {
   };
   const handleLogOut = () => {
     setAnchorEl(null);
-    //  console.log(JSON.stringify(signUp));
     const deleteLogin = async () => {
       const res = await fetch("/sessions", {
         method: "DELETE",
@@ -55,74 +55,81 @@ export default function NavBar(props) {
   };
 
   return (
-    <AppBar color="inherit">
-      <Toolbar>
-        <Typography className={classes.logo} style={{ fontWeight: 700 }}>
+    <AppBar color="inherit" elevation={2}>
+      <Container>
+        <Toolbar>
           <Link
             component={RouterLink}
             color="inherit"
             to="/"
             style={{ textDecoration: "none" }}
           >
-            Dog Tinder
+            <img
+              src="https://i.ibb.co/gD9cMM8/logosmall.png"
+              height="30"
+              className={classes.logo}
+              alt="logo"
+            />
           </Link>
-        </Typography>
-        <Typography className={classes.links}>
-          <Link
-            component={RouterLink}
-            color="inherit"
-            to="/browse"
-            style={{ textDecoration: "none" }}
-          >
-            Browse
-          </Link>
-        </Typography>
-        <Typography className={classes.title}>
-          <Link
-            component={RouterLink}
-            color="inherit"
-            to="/matches"
-            style={{ textDecoration: "none" }}
-          >
-            Matches
-          </Link>
-        </Typography>
-        <Typography>
-          Howdy,{" "}
-          {props.currentUser?.fullName ? props.currentUser?.fullName : "User"}
-        </Typography>
-        <div className={classes.avatar}>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={open}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
-            <MenuItem onClick={handleClose}>Account Settings</MenuItem>
-            <MenuItem onClick={handleLogOut}>Log Out</MenuItem>
-          </Menu>
-        </div>
-      </Toolbar>
+
+          <Typography className={classes.links}>
+            <Link
+              component={RouterLink}
+              color="inherit"
+              to="/browse"
+              style={{ textDecoration: "none" }}
+            >
+              Browse
+            </Link>
+          </Typography>
+          <Typography className={classes.title}>
+            <Link
+              component={RouterLink}
+              color="inherit"
+              to="/matches"
+              style={{ textDecoration: "none" }}
+            >
+              Matches
+            </Link>
+          </Typography>
+          <Typography>
+            Howdy,{" "}
+            {props.currentUser?.fullName ? props.currentUser?.fullName : "User"}
+            !
+          </Typography>
+          <div className={classes.avatar}>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <Avatar src={props.currentUser.image} />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Account Settings</MenuItem>
+              <MenuItem onClick={handleLogOut}>Log Out</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
